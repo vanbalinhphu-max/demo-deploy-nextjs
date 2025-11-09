cccd64568b395936d961ca1a48e6bc706db09a10
vanbalinhphu-max <vanbalinhphu@gmail.com>
2025-11-09 14:49:38 +0700

    fix

--- src/app/cached-data-2/action.js
+++
@@ -1,9 +0,0 @@
-'use server'
-
-import { revalidateTag } from "next/cache"
-import { redirect } from "next/navigation"
-
-export async function clearTimestampCache(){
-    revalidateTag('timestamp');
-    redirect('/cached-data');
-}
