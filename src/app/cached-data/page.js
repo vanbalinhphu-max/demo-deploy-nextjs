114867102a8e0e24a53660fa221b7bc549886cc5
vanbalinhphu-max <vanbalinhphu@gmail.com>
2025-11-09 15:15:23 +0700

    fix

--- src/app/cached-data/page.js
+++ src/app/cached-data/page.js
@@ -1,27 +1,32 @@
 import { clearTimestampCache } from "./action";
-const BASE_URL =
-    process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000'
 
 async function fetchData() {
-    console.log("fetching data");
-    const res = await fetch(`${BASE_URL}/api/timestamp`, {
-        cache: "force-cache",
-        next: { tags:['timestamp']}
-    })
-    if (!res.ok) {
-        throw new Error('failed to fetch data');
-    }
-    return res.json();
+  console.log("fetching data");
+  const res = await fetch(`${baseUrl}/api/timestamp`, {
+    cache: "force-cache",
+    next: { tags: ["timestamp"] },
+  });
+  if (!res.ok) {
+    throw new Error("failed to fetch data");
+  }
+  return res.json();
 }
 export default async function CachedDataPage() {
-    const data1 = await fetchData();
-    return (
-        <div>
-            <h1>Dữ liệu đã được cache</h1>
-            <p>Timestamp1 : {data1.timestamp}</p>
-            <form action={clearTimestampCache}>
-                <button>Xóa cache timestamp tag</button>
-            </form>
-        </div>
-    )
+  const data1 = await fetchData();
+  return (
+    <div>
+      <h1>Dữ liệu đã được cache</h1>
+      <p>Timestamp1 : {data1.timestamp}</p>
+      <form action={clearTimestampCache}>
+        <button>Xóa cache timestamp tag</button>
+      </form>
+    </div>
+  );
 }
